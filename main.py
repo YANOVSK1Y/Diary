63189ca7fb28c84529a14863987089491126c799
YANOVSK1Y <andreyianovskyi78@gmail.com>
2021-07-27 13:22:45 +0300

    Added menu

--- main.py
+++ main.py
@@ -8,11 +8,25 @@ class Diary(Frame):
         Frame.__init__(self, window)
         self.dirlist = list()
         self.main_folder = str()
+        self._init_window(window)
 
 
-    def _init_window(self):
-        pass
+    def _init_window(self, window):
 
+        # create menu
+        self.mainmenu = Menu(window)
+        window.config(menu=self.mainmenu)
+        helpmenu = Menu(self.mainmenu, tearoff=0)
+        helpmenu.add_command(label='File')
+        helpmenu.add_command(label='Folder')
+        self.mainmenu.add_cascade(label = 'Open', menu=helpmenu)
+        self.mainmenu.add_command(label='Save')
+
+        #create labels for brows and addit text
+        self.label_dir = tkinter.Label(text='', bg='grey', width=20, height=25)
+        self.label_dir.place(x=50, y=50)
+        self.label_text = tkinter.Text(bg='grey', width=40, height=25)
+        self.label_text.place(x=250, y = 50)
 
     def _get_list_dir(self):
         for item in os.listdir():
